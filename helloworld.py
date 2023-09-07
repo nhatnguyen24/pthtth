3a3d38a4e3d4276c0d6072e74a689f362106236a
nhatnguyen24 <98161283+nhatnguyen24@users.noreply.github.com>
2023-09-07 19:06:15 +0700

    python co ban

---
+++ helloworld.py
@@ -0,0 +1,32 @@
+print('Hello World')
+#2.1
+a = 1
+print(a)
+#2.2
+a= 5
+b= 20 
+print(a + b)
+print(a - b)
+print(a / b)
+print(a * b)
+#2.3
+a = 5
+b = 1
+if a <b :
+    print(a)
+else :
+    print(b)
+#2.4
+count = 0
+while (count < 9):
+    print ('The count is:', count)
+    count = count + 1
+#2.5
+def sum(a, b):
+    return (a+b)
+
+result = sum(1,2)
+print(result)
+#2.6
+count = len("Nguyen Minh Nhat")
+print(count)
